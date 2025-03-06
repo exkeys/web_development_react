2afc97de64833bcc7c16accb8e3622488f47a225
exkeys <rjsdn6422@naver.com>
2025-03-06 21:58:05 +0900

    feat: 20250306 weather / github api uploaded

--- weatherapp/src/App.jsx
+++ weatherapp/src/App.jsx
@@ -1,39 +1,41 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
-import { useEffect } from 'react';
 
 function App() {
+  
   const [weather, setWeather] = useState({
     temp: '',
     desc: '',
     icon: '',
-
   });
 
   useEffect(() => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&units=Metric&APIkey=bdff61d6ba94aeaa2e47bed54157fca6`)
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Seoul&units=Metric&APIkey=08315439ef75a9358a09e997111e70c9')
     .then(response => response.json())
     .then(result => {
-      setWeather({temp: result.main.temp,
+      setWeather({
+        temp: result.main.temp,
         desc: result.weather[0].description,
         icon: result.weather[0].icon,
-      })
+      });
     })
     .catch(err => console.log(err))
-  })
-  if(weather.icon){
+  }, []);
+
+  if(weather.icon) {
     return (
       <>
         <p>온도 : {weather.temp} 도</p>
-        <p>설명 : {weather.desc} 도</p>
-        <img src={`http://openweathermap.org/img/wn/${weather.icon}@2.png`} alt="날씨 아이콘" />
-        
+        <p>설명 : {weather.desc}</p>
+        <img src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="날씨 아이콘" />
       </>
-    )
+    );
   }
-  else{
+  else {
     return <div>로딩 중...</div>
   }
+
+
 }
 
 export default App
